41caee6bad13785ddf93395856f5a49b0bb8d1d4
Fabio Bandacheski <fabio_bandacheskii@econeteditora.com.br>
2022-03-28 10:47:30 -0300

    migration phone to string

--- database/migrations/2022_03_16_020440_create_tickets_table.php
+++ database/migrations/2022_03_16_020440_create_tickets_table.php
@@ -16,7 +16,7 @@ class CreateTicketsTable extends Migration
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('phone');
+            $table->string('phone');
             $table->timestamps();
         });
     }
